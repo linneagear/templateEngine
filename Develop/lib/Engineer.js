79a5a2a62e6ce3e5bf2881897e9a565317559f77
Linnea <linneagear@gmail.com>
2020-06-06 10:33:24 -0700

    Added questions to app.js, inquirer prompt, and employee.js functions

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,11 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(id, github) {
+        super(name, id, email);
+        this.github = github;
+    }  
+
+    
+}
