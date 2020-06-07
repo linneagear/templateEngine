b968136f0286e7eab59d6e8db270edcf2f8d968d
Linnea <linneagear@gmail.com>
2020-06-06 17:14:05 -0700

    Got a working render function, that adds team.html

--- Develop/app.js
+++ Develop/app.js
@@ -20,98 +20,101 @@ let teamList = [];
 // ask questions, THEN create and push to teamList, THEN call addMember function
 function addManager() {
     inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your id number?"
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is your office phone number?"
-    }
-]).then(function(answers) {
-    const manager = new Manager(answers.name, parseInt(answers.id), answers.email, answers.officeNumber)
-    teamList.push(manager)
-    console.log(teamList);
-    addMember();
-})
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your id number?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office phone number?"
+        }
+    ]).then(function (answers) {
+        const manager = new Manager(answers.name, parseInt(answers.id), answers.email, answers.officeNumber)
+        teamList.push(manager)
+        console.log(teamList);
+        addMember();
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 }
 
 function addEngineer() {
     inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "What is your engineer's name?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your engineer's email?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your engineer's id number?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your engineer's github username?"
-    }
-]).then(function(answers) {
-    // add new Engineer variable
-    const engineer = new Engineer(answers.name, parseInt(answers.id), answers.email, answers.github);
-    // push to team members list
-    teamList.push(engineer);
-    console.log(teamList);
-    // run addMember function 
-    addMember();
-})
+        {
+            type: "input",
+            name: "name",
+            message: "What is your engineer's name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your engineer's email?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your engineer's id number?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineer's github username?"
+        }
+    ]).then(function (answers) {
+        // add new Engineer variable
+        const engineer = new Engineer(answers.name, parseInt(answers.id), answers.email, answers.github);
+        // push to team members list
+        teamList.push(engineer);
+        console.log(teamList);
+        // run addMember function 
+        addMember();
+    })
 }
 
 function addIntern() {
     inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "What is your intern's name?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your intern's email?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your intern's id number?"
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What school is your intern attending?"
-    }
-]).then(function(answers) {
-    // add new intern variable
-    const intern = new Intern(answers.name, parseInt(answers.id), answers.email, answers.school);
-    // push to team members list
-    teamList.push(intern);
-    console.log(teamList);
-    // run addMember function 
-    addMember();
-})
+        {
+            type: "input",
+            name: "name",
+            message: "What is your intern's name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your intern's email?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your intern's id number?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school is your intern attending?"
+        }
+    ]).then(function (answers) {
+        // add new intern variable
+        const intern = new Intern(answers.name, parseInt(answers.id), answers.email, answers.school);
+        // push to team members list
+        teamList.push(intern);
+        console.log(teamList);
+        // run addMember function 
+        addMember();
+    })
 }
 
 // function to add a new member, or choose not to add anymore
@@ -128,26 +131,34 @@ function addMember() {
             ]
 
         }
-    ]).then(function(answer) {
+    ]).then(function (answer) {
         // if engineer, addEngineer
-        if(answer.type === "Engineer") {
+        if (answer.type === "Engineer") {
             addEngineer();
-        // else if intern, add intern
+            // else if intern, add intern
         } else if (answer.type === "Intern") {
             addIntern();
         }
         // otherwise, render the teamList
         else {
-            render(teamList)
+            const renderTeam = render(teamList);
+            writeToFile(renderTeam);
         }
     })
 }
 
+
+function writeToFile(data) {
+    // want to save the markdown file to the repo
+    fs.writeFile("./output/team.html", data, function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log('HTML created! Check your output file!');
+    })
+}
+
 addManager();
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
